6cb79232bda4971dd08823e627096b8e538f0f11
massivelines <shawn@massivelines.com>
2017-09-08 18:53:28 -0500

    Body parser setup, callback pages post to callback express

--- node.js
+++ node.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var http = require('http');
 
 var app = express();
 
@@ -8,7 +9,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -22,44 +25,41 @@ app.all('*', function(req, res, next) {
   next();
 });
 
-app.post('/', function(request, response){
-  console.log(request.body);      // your JSON
-   response.send(request.body);    // echo the result back
+
+app.get('/login', function(req, res) {
+  console.log('login');
+
+  // TODO: convert to system vars
+  var scopes = ['user-read-playback-state'];
+  var client_id = '8d2ca6ffda244def9852f84650c2bfa2'; // Your client id
+  var client_secret = '5e1882b5faba4be4b3db73e9b72ac565';
+  var redirect_uri = 'http://localhost:5000/callback.html'; // Your redirect uri
+
+  var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
+    '&redirect_uri=' + encodeURIComponent(redirect_uri) +
+    '&scope=' + encodeURIComponent(scopes.join(' ')) +
+    '&response_type=code&show_dialog=true';
+  res.redirect(url);
 });
 
-// app.use(function (req, res) {
-//   res.setHeader('Content-Type', 'application/json');
-//   res.write('you posted:\n');
-//   res.end(JSON.stringify(req.body, null, 2));
-//   console.log(req.body);
-// });
+app.post('/callback', function(req, res) {
+  var data = req.body;
 
 
-// app.get('/login', function(req, res) {
-//   console.log('login');
-//   var scopes = ['user-read-playback-state'];
-//   var client_id = '8d2ca6ffda244def9852f84650c2bfa2'; // Your client id
-//   var redirect_uri = 'http://localhost:5000/callback.html'; // Your redirect uri
-//
-//   var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
-//     '&redirect_uri=' + encodeURIComponent(redirect_uri) +
-//     '&scope=' + encodeURIComponent(scopes.join(' ')) +
-//     '&response_type=code&show_dialog=true';
-//   res.redirect(url);
-// });
 
-// app.get('/callback', function(req, res) {
-  // console.log('called');
-  // res.send('Hello World!');
-  // var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  // console.log(fullUrl);
-  // res.end();
-// });
 
-app.post('/test', function(req, res) {
-  console.log(req.body); // populated!
-  res.send(req.body);
+  console.log(data);
+  console.log('callback called');
+  res.sendStatus(200);
 });
+
+
+
+// app.post('/test', function(req, res) {
+//   var test = req.body;
+//   console.log(test);
+//   res.send(test);
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
--- public/app.js
+++ public/app.js
@@ -7,52 +7,59 @@ var SpotifyHeroku = function(scopes) {
 
 
 
-  this.login = function() {
+  this.login = function(scopes) {
+
+    // console.log(test);
+
+    // fetch('http://localhost:5000/test', {
+    //   method: 'POST',
+    //   body: JSON.stringify(scopes),
+    //   // mode: 'cors',
+    //   headers: {
+    //     // 'Accept': 'application/json',
+    //     'Content-Type': 'application/json'
+    //   }
+    // }).then(function(response) {
+    //   return response.json();
+    // }).then(function(data) {
+    //   console.log(data);
+    // }).catch(function(err) {
+    //   // Error :(
+    //   console.log(err);
+    //   console.log('error');
+    // });
+
+    // TEMP: for local testing
+    var url = 'http://localhost:5000/login';
+
+    // var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
+    //   '&redirect_uri=' + encodeURIComponent(redirect_uri) +
+    //   '&scope=' + encodeURIComponent(scopes.join(' ')) +
+    //   '&response_type=code&show_dialog=true';
+
+    var width = 450,
+      height = 730,
+      left = screen.width / 2 - width / 2,
+      top = screen.height / 2 - height / 2;
+
+    var w = window.open(
+      url,
+      'Spotify',
+      'menubar=no,location=no,resizable=no,scrollbars=no,status=no,' +
+      ' width=' + width +
+      ', height=' + height +
+      ', top=' + top +
+      ', left=' + left
+    );
+
+
     var test = {
       tester: 'maybe',
       yep: 'nope'
     };
-    // console.log(test);
 
-    fetch('http://localhost:5000/test', {
-    method: 'POST',
-    body: JSON.stringify(test),
-    mode: 'cors',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }).then(function(response) {
-    // console.log(response);
-	  //  console.log('success');
-}).catch(function(err) {
-	// Error :(
-  // console.log(err);
-  // console.log('error');
-});
-//
-//     var url = 'http://localhost:5000/login';
-//
-//     // var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
-//     //   '&redirect_uri=' + encodeURIComponent(redirect_uri) +
-//     //   '&scope=' + encodeURIComponent(scopes.join(' ')) +
-//     //   '&response_type=code&show_dialog=true';
-//
-//     // var width = 450,
-//     //   height = 730,
-//     //   left = screen.width / 2 - width / 2,
-//     //   top = screen.height / 2 - height / 2;
-//
-//     // var w = window.open(
-//     //   url,
-//     //   'Spotify',
-//     //   'menubar=no,location=no,resizable=no,scrollbars=no,status=no,' +
-//     //   ' width=' + width +
-//     //   ', height=' + height +
-//     //   ', top=' + top +
-//     //   ', left=' + left
-//     // );
-//
+    // TODO: return token and data
+    return test;
   };
 
 };
@@ -61,9 +68,6 @@ var SpotifyHeroku = function(scopes) {
 window.addEventListener('message', function(event) {
   var hash = JSON.parse(event.data);
   console.log(hash);
-  // if (hash.type == 'access_token') {
-  //   resolve(hash.access_token);
-  // }
 }, false);
 
 // login
@@ -89,7 +93,7 @@ window.addEventListener('message', function(event) {
 // -return success
 // -launch login popup
 // -send code ect to node throuh fetch
-// -send token to browser by parsebody
+// -send token to browser by parsebody????
 
 // refresh
 // -call function pass vars
