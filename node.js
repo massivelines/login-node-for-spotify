d87412e842786b740081b589f820f6c865a15059
massivelines <shawn@massivelines.com>
2017-09-09 18:52:18 -0500

    Using local storage and websockets
    Using websockets to transfer the data then local storage to store the
    tokens

--- node.js
+++ node.js
@@ -3,8 +3,12 @@ var bodyParser = require("body-parser");
 var http = require('http');
 var request = require('request');
 var rp = require('request-promise');
+var expressWs = require('express-ws');
 
 var app = express();
+expressWs(app);
+
+
 
 // TODO: convert to system vars
 var client_id = '8d2ca6ffda244def9852f84650c2bfa2'; // Your client id
@@ -14,11 +18,13 @@ var redirect_uri = 'http://localhost:5000/callback.html'; // Your redirect uri
 var allowedConnections = ['http://localhost:5000', 'http://localhost:7883'];
 
 var host;
-var scopes;
+var scopes = [];
+var token;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -37,70 +43,156 @@ app.all('*', function(req, res, next) {
   next();
 });
 
-app.post('/data', function (req, res) {
-  console.log(req.body);
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+var state;
+
+// data pulled host calling requests and scopes
+app.post('/data', function(req, res) {
+  // console.log(req.body);
   host = req.body.hostURL;
   scopes = req.body.scopes;
-  res.sendStatus(200);
+  res.end();
 });
 
+// redirect for the intial login popup
 app.get('/login', function(req, res) {
-  console.log('login');
-  var scopes = ['user-read-playback-state'];
-  var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
-    '&redirect_uri=' + encodeURIComponent(redirect_uri) +
-    '&scope=' + encodeURIComponent(scopes.join(' ')) +
-    '&response_type=code&show_dialog=true';
-  res.redirect(url);
+  token = null;
+  state = generateRandomString(16);
+
+  // TODO error out if never filled
+  // loop that makes sure scopes is populated before calling for the redirect
+  function loginRedirect() {
+    if (scopes.length > 0) {
+      var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
+        '&redirect_uri=' + encodeURIComponent(redirect_uri) +
+        '&scope=' + encodeURIComponent(scopes.join(' ')) +
+        '&response_type=code'+
+        '&state=' +state+
+        '&show_dialog=true';
+      res.redirect(url);
+    } else {
+      setTimeout(function() {
+        loginRedirect();
+      }, 500);
+    }
+  }
+  loginRedirect();
+
 });
 
+// callback from callback.html to pass spotify response codes to turn into token
 app.post('/callback', function(req, res) {
-  console.log('callback called');
-
+  // console.log('callback called');
   var data = req.body;
-  var authOptions = {
-    method: 'POST',
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: data.code,
-      redirect_uri: redirect_uri,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-    },
-    json: true
-  };
 
-  rp(authOptions).
-  then(function(body) {
+  // test if state is sent back the same
+  if (data.state == state) {
+    var authOptions = {
+      method: 'POST',
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: data.code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
 
-    var access_token = body.access_token;
-    var refresh_token = body.refresh_token;
-    var expires_in = body.expires_in;
+    // convert code into token for intial login
+    rp(authOptions).
+    then(function(body) {
 
-    console.log(body);
-    res.sendStatus(200);
-
-  }).catch(function(err) {
-    console.log(err);
-  });
+      token = {
+        access_token: body.access_token,
+        refresh_token: body.refresh_token,
+        expires_in: body.expires_in
+      };
+      console.log(token);
+      // send status code to close popup
+      res.sendStatus(200);
+    }).catch(function(err) {
+      // TODO: when error redirect to login
+      console.log(err);
+    });
+  }
 
 });
 
-function sendData() {
+// sends token to websocket js file
+app.ws('/token', function (ws, req) {
+  // TODO error out if never filled
+  function sendToken() {
+    if( typeof token !== 'undefined' || typeof token !== null){
+      // console.log(token);
+      ws.send(JSON.stringify(token));
+    } else {
+      setTimeout(sendToken, 500);
+    }
+
+  }
+
+  sendToken();
+});
+
+// refreshes the token called by the websocket
+app.ws('/refresh', function (ws, req) {
+  ws.on('message', function (old_token) {
+    old_token = JSON.parse(old_token);
+    // console.log(old_token.refresh_token);
+    var data = req.body;
+    var authOptions = {
+      method: 'POST',
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        refresh_token: old_token.refresh_token,
+        grant_type: 'refresh_token'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    // convert code into token
+    rp(authOptions).
+    then(function(body) {
+
+      var newToken = {
+        access_token: body.access_token,
+      };
+
+      ws.send(JSON.stringify(newToken));
+    }).catch(function(err) {
+      // TODO: when error redirect to login
+      console.log(err);
+    });
+
+  });
+});
 
 
-}
-
-
-
-// app.post('/test', function(req, res) {
-//   var test = req.body;
-//   console.log(test);
-//   res.send(test);
-// });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+// TODO set up security tests for all incoming and outgoing connections/posts
+// for websocket
+// var ip = req.headers.origin;
+// console.log(ip);
--- public/app.js
+++ public/app.js
@@ -1,36 +1,46 @@
-var SpotifyHeroku = function(scopes, nodeHost) {
-  this.scopes = scopes;
+var SpotifyHeroku = function() {
 
   var windowURL = window.location.origin;
+
+  function init() {
+
+  }
+  // sends scopes and main url of page to node server
+  fetch(nodeHost + '/data', {
+    method: 'POST',
+    body: JSON.stringify({
+      scopes: scopes,
+      hostURL: windowURL
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).catch(function(err) {
+    // Error :(
+    console.log(err);
+    console.log('error');
+  });
+
+  function storage(token) {
+    localStorage.setItem('access_token', token.access_token);
+    localStorage.setItem('expires_in', token.expires_in);
+    localStorage.setItem('refresh_token', token.refresh_token);
+  }
 
 
   this.login = function() {
 
-    fetch('http://localhost:5000/data', {
-      method: 'POST',
-      body: JSON.stringify({
-        scopes: scopes,
-        hostURL: windowURL
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(function(response) {
-      console.log(response);
-    }).catch(function(err) {
-      // Error :(
-      console.log(err);
-      console.log('error');
-    });
-
-    var url = 'http://localhost:5000/login';
+    // then opens a popup window to login into, that sends info back to the node server
+    var url = nodeHost + '/login';
 
     var width = 450,
       height = 730,
       left = screen.width / 2 - width / 2,
       top = screen.height / 2 - height / 2;
 
-    var w = window.open(
+    var popup;
+
+    popup = window.open(
       url,
       'Spotify',
       'menubar=no,location=no,resizable=no,scrollbars=no,status=no,' +
@@ -40,40 +50,34 @@ var SpotifyHeroku = function(scopes, nodeHost) {
       ', left=' + left
     );
 
-  };
+    // when the popu closes, then get the tokens
+    function popupClosed() {
+      if (popup.closed) {
+        // TODO secure websockets
+        // var HOST = nodeHost.replace(/^http/, 'ws');
+        var ws = new WebSocket('ws://localhost:5000/token');
 
+        ws.onmessage = function(message) {
+          var token = JSON.parse(message.data);
+          storage(token);
+          ws.close();
+        };
+      } else {
+        setTimeout(popupClosed, 500);
+      }
+    }
+    popupClosed();
+
+
+
+  }; //--------------end of login
+
+
+  init();
 };
 
 
-window.addEventListener('message', function(event) {
-  var hash = JSON.parse(event.data);
-  console.log(hash);
-}, false);
 
-// this.login = function() {
-//   console.log(scopes);
-//   var test = {
-//     tester: 'maybe',
-//     yep: 'nope'
-//   };
-//
-//   fetch('http://localhost:5000/data', {
-//     method: 'POST',
-//     body: JSON.stringify(scopes),
-//     // mode: 'cors',
-//     headers: {
-//       // 'Accept': 'application/json',
-//       'Content-Type': 'application/json'
-//     }
-//   }).then(function(response) {
-//     return response.json();
-//   }).then(function(data) {
-//     console.log(data);
-//   }).catch(function(err) {
-//     // Error :(
-//     console.log(err);
-//     console.log('error');
-//   });
 
 // login
 // need to open popup from node?????
