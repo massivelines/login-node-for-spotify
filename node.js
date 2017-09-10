572e96f78f2993a8f859d32fcc2ff72b088ab59f
massivelines <shawn@massivelines.com>
2017-09-09 23:11:13 -0500

    heroku test, error crash, commented everything out

--- node.js
+++ node.js
@@ -15,11 +15,11 @@ var client_id = '8d2ca6ffda244def9852f84650c2bfa2'; // Your client id
 var client_secret = '5e1882b5faba4be4b3db73e9b72ac565';
 var redirect_uri = 'http://localhost:5000/callback.html'; // Your redirect uri
 
-var allowedConnections = ['http://localhost:5000', 'http://localhost:7879'];
-
-var host;
-var scopes = [];
-var token;
+// var allowedConnections = ['http://localhost:5000', 'http://localhost:7879'];
+//
+// var host;
+// var scopes = [];
+// var token;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -43,157 +43,157 @@ app.all('*', function(req, res, next) {
   next();
 });
 
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-var state;
-
-// data pulled host calling requests and scopes
-app.post('/data', function(req, res) {
-  // console.log(req.body);
-  host = req.body.hostURL;
-  scopes = req.body.scopes;
-  res.end();
-});
-
-// redirect for the intial login popup
-app.get('/login', function(req, res) {
-  token = null;
-  state = generateRandomString(16);
-
-  // TODO error out if never filled
-  // loop that makes sure scopes is populated before calling for the redirect
-  function loginRedirect() {
-    if (scopes.length > 0) {
-      var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
-        '&redirect_uri=' + encodeURIComponent(redirect_uri) +
-        '&scope=' + encodeURIComponent(scopes.join(' ')) +
-        '&response_type=code'+
-        '&state=' +state+
-        '&show_dialog=true';
-      res.redirect(url);
-    } else {
-      setTimeout(function() {
-        loginRedirect();
-      }, 500);
-    }
-  }
-  loginRedirect();
-
-});
-
-// callback from callback.html to pass spotify response codes to turn into token
-app.post('/callback', function(req, res) {
-  // console.log('callback called');
-  var data = req.body;
-
-  // test if state is sent back the same
-  if (data.state == state) {
-    var authOptions = {
-      method: 'POST',
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: data.code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-
-    // convert code into token for intial login
-    rp(authOptions).
-    then(function(body) {
-
-      token = {
-        access_token: body.access_token,
-        refresh_token: body.refresh_token,
-        expires_in: body.expires_in
-      };
-      // console.log(token);
-      // send status code to close popup
-      res.sendStatus(200);
-    }).catch(function(err) {
-      // TODO: when error redirect to login
-      console.log(err);
-    });
-  }
-
-});
-
-// sends token to websocket js file
-app.ws('/token', function (ws, req) {
-  // TODO error out if never filled
-  function sendToken() {
-    if( typeof token !== 'undefined' || typeof token !== null){
-      // console.log(token);
-      ws.send(JSON.stringify(token));
-    } else {
-      setTimeout(sendToken, 500);
-    }
-
-  }
-
-  sendToken();
-});
-
-// refreshes the token called by the websocket
-app.ws('/refresh', function (ws, req) {
-  ws.on('message', function (old_token) {
-
-    old_token = JSON.parse(old_token);
-    // console.log(old_token);
-
-    var data = req.body;
-    var authOptions = {
-      method: 'POST',
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        // refresh_token: 'old_token',
-        refresh_token: old_token,
-        grant_type: 'refresh_token'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-
-    // convert code into token
-    rp(authOptions).
-    then(function(body) {
-
-      var newToken = {
-        access_token: body.access_token,
-      };
-
-      ws.send(JSON.stringify(newToken));
-    }).catch(function(err) {
-      // TODO: when error redirect to login
-
-      console.log(err);
-      var error = {
-        statusCode: err.statusCode,
-        message: err.message
-      };
-      ws.send(JSON.stringify(error));
-    });
-
-  });
-});
+// /**
+//  * Generates a random string containing numbers and letters
+//  * @param  {number} length The length of the string
+//  * @return {string} The generated string
+//  */
+// var generateRandomString = function(length) {
+//   var text = '';
+//   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//
+//   for (var i = 0; i < length; i++) {
+//     text += possible.charAt(Math.floor(Math.random() * possible.length));
+//   }
+//   return text;
+// };
+// var state;
+//
+// // data pulled host calling requests and scopes
+// app.post('/data', function(req, res) {
+//   // console.log(req.body);
+//   host = req.body.hostURL;
+//   scopes = req.body.scopes;
+//   res.end();
+// });
+//
+// // redirect for the intial login popup
+// app.get('/login', function(req, res) {
+//   token = null;
+//   state = generateRandomString(16);
+//
+//   // TODO error out if never filled
+//   // loop that makes sure scopes is populated before calling for the redirect
+//   function loginRedirect() {
+//     if (scopes.length > 0) {
+//       var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
+//         '&redirect_uri=' + encodeURIComponent(redirect_uri) +
+//         '&scope=' + encodeURIComponent(scopes.join(' ')) +
+//         '&response_type=code'+
+//         '&state=' +state+
+//         '&show_dialog=true';
+//       res.redirect(url);
+//     } else {
+//       setTimeout(function() {
+//         loginRedirect();
+//       }, 500);
+//     }
+//   }
+//   loginRedirect();
+//
+// });
+//
+// // callback from callback.html to pass spotify response codes to turn into token
+// app.post('/callback', function(req, res) {
+//   // console.log('callback called');
+//   var data = req.body;
+//
+//   // test if state is sent back the same
+//   if (data.state == state) {
+//     var authOptions = {
+//       method: 'POST',
+//       url: 'https://accounts.spotify.com/api/token',
+//       form: {
+//         code: data.code,
+//         redirect_uri: redirect_uri,
+//         grant_type: 'authorization_code'
+//       },
+//       headers: {
+//         'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+//       },
+//       json: true
+//     };
+//
+//     // convert code into token for intial login
+//     rp(authOptions).
+//     then(function(body) {
+//
+//       token = {
+//         access_token: body.access_token,
+//         refresh_token: body.refresh_token,
+//         expires_in: body.expires_in
+//       };
+//       // console.log(token);
+//       // send status code to close popup
+//       res.sendStatus(200);
+//     }).catch(function(err) {
+//       // TODO: when error redirect to login
+//       console.log(err);
+//     });
+//   }
+//
+// });
+//
+// // sends token to websocket js file
+// app.ws('/token', function (ws, req) {
+//   // TODO error out if never filled
+//   function sendToken() {
+//     if( typeof token !== 'undefined' || typeof token !== null){
+//       // console.log(token);
+//       ws.send(JSON.stringify(token));
+//     } else {
+//       setTimeout(sendToken, 500);
+//     }
+//
+//   }
+//
+//   sendToken();
+// });
+//
+// // refreshes the token called by the websocket
+// app.ws('/refresh', function (ws, req) {
+//   ws.on('message', function (old_token) {
+//
+//     old_token = JSON.parse(old_token);
+//     // console.log(old_token);
+//
+//     var data = req.body;
+//     var authOptions = {
+//       method: 'POST',
+//       url: 'https://accounts.spotify.com/api/token',
+//       form: {
+//         // refresh_token: 'old_token',
+//         refresh_token: old_token,
+//         grant_type: 'refresh_token'
+//       },
+//       headers: {
+//         'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+//       },
+//       json: true
+//     };
+//
+//     // convert code into token
+//     rp(authOptions).
+//     then(function(body) {
+//
+//       var newToken = {
+//         access_token: body.access_token,
+//       };
+//
+//       ws.send(JSON.stringify(newToken));
+//     }).catch(function(err) {
+//       // TODO: when error redirect to login
+//
+//       console.log(err);
+//       var error = {
+//         statusCode: err.statusCode,
+//         message: err.message
+//       };
+//       ws.send(JSON.stringify(error));
+//     });
+//
+//   });
+// });
 
 
 
